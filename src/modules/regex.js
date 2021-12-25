28fb2441c6b99d1d94eee81a143d32de87adb40a
Egleman <martsnov@hotmail.com>
2021-12-25 18:21:07 +0300

    домашка 21

--- src/modules/regex.js
+++ src/modules/regex.js
@@ -8,15 +8,13 @@ const regex = () => {
     const inputNameBottom = document.querySelector('#form2-name');
     const inputMessageBottom = document.querySelector('#form2-message');
 
-    function titleCase(str) {
-        var splitStr = str.toLowerCase().split(' ');
-        for (var i = 0; i < splitStr.length; i++) {
-            // You do not need to check if i is larger than splitStr length, as your for does that for you
-            // Assign it back to the array
-            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-        }
-        // Directly return the joined string
-        return splitStr.join(' '); 
+
+     function customTrim(val) {
+         val = val.replace(/\s+/g, " ");
+         val = val.replace(/-+/g, '-');
+         val = val.replace(/^[ |\-+]/g, '');
+         val = val.replace(/[ |\-+]$/g, '');
+         return val;
      }
 
     inputsCalc.forEach((input) => {
@@ -26,51 +24,50 @@ const regex = () => {
     });
 
     inputName.forEach((input) => {
-        input.addEventListener('blur', () => {
-            //debugger;
-            let reg = /[^А-Яа-яёЁ \-]/g;
-            //let inp = '';
-            if (reg.test(input.value)) {
-                //debugger;
-                input.value = input.value.replace(reg, '').replace(/(^\s-*)|(\s-*)$/g, ''); 
-                console.log(input.value);
-                //input.value = '';
-            } else {
-                input.value = titleCase(input.value.trim().replace(/(^\s*-)|(\s*-)$/g, ''));
-            }
-            //input.value = input.value.replace(/[^а-яё -]/iu, '');
+        input.addEventListener('blur', (e) => {
+            let val = e.target.value;
+            val = val.replace(/[^а-яё \-]/ig, '');
+            val = customTrim(val);
+            val = val.replace(/( |^)[ а-яё]/g, u => u.toUpperCase());
+            e.target.value = val;
         });
     });
 
     inputEmail.forEach((input) => {
-        input.addEventListener('blur', () => {
-            if(/[^A-Za-z-@!.*~']/.test(input.value)) {
-                input.value = input.value.replace(/[^A-Za-z-@!.*~']/g, '');
-            } else {
-                input.value = input.value.replace(/(^\s*-)|(\s*-)$/g, '');
-            }
+        input.addEventListener('blur', (e) => {
+            let val = e.target.value;
+            val = val.replace(/[^A-Za-z-@!.*~']/g, '');
+            val = customTrim(val);
+            e.target.value = '';
+            e.target.value = val;
         });
     });
 
 
-
     inputPhone.forEach((input) => {
-        input.addEventListener('blur', () => {
-            if (/[^\d-)(]/.test(input.value)) {
-                input.value = input.value.replace(/[^\d-)(]/g, '');
-            } else {
-                input.value = input.value.trim().replace(/(^\s*-)|(\s*-)$/g, '');
-            }
-            //input.value = input.value.replace(/[^\d-)(]/g, '');
+        input.addEventListener('blur', (e) => {
+            let val = e.target.value;
+            val = val.replace(/[^\d-)(]/g, '');
+            val = customTrim(val);
+            e.target.value = '';
+            e.target.value = val;
         }); 
     });
 
-    inputNameBottom.addEventListener('input', () => {
-        inputNameBottom.value = inputNameBottom.value.replace(/[^а-яё -]/iu, '');
+    inputNameBottom.addEventListener('blur', (e) => {
+        let val = e.target.value;
+        val = val.replace(/[^а-яё \-]/ig, '');
+        val = customTrim(val);
+        val = val.replace(/( |^)[ а-яё]/g, u => u.toUpperCase());
+        e.target.value = val;
     });
 
-    inputMessageBottom.addEventListener('input', () => {
-        inputMessageBottom.value = inputMessageBottom.value.replace(/[^а-яё -]/iu, '');
+    inputMessageBottom.addEventListener('blur', (e) => {
+        let val = e.target.value;
+        val = val.replace(/[^а-яё \-]/ig, '');
+        val = customTrim(val);
+        e.target.value = '';
+        e.target.value = val;
     });
 
 };
