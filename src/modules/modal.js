ee62f1078e21046b24bbe6b505188552b5a2466c
Egleman <martsnov@hotmail.com>
2022-01-07 20:55:11 +0300

    правки

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,16 +2,40 @@ import { animate } from './helpers';
 const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn');
     const modal = document.querySelector('.popup');
+    function blockBody() {
+        const body = document.body;
+        body.style.overflow = 'hidden';
+        const bodyScroll = calcScroll();
+        body.style.marginRight = `${bodyScroll}px`;
+    }
+
+    function unBlockBody() {
+        const body = document.body;
+        body.style.overflow = 'auto';
+        body.style.marginRight = `0`;
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+        div.style.width = '500px';
+        div.style.height = '500px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth;
+    }
 
     buttons.forEach((btn) => {
         btn.addEventListener('click', () => {
             //Проверка размера экрана
             if(window.screen.width < 768) {
                 modal.style.display = 'block';
-                document.body.style.overflow = 'hidden';
+                blockBody();
             } else {
                 modal.style.display = 'block';
-                document.body.style.overflow = 'hidden';
+                blockBody();
                 modal.style.opacity = '0';
                 animate({
                     duration: 500,
@@ -29,7 +53,7 @@ const modal = () => {
     modal.addEventListener('click', (e) => {
         if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
             modal.style.display = 'none';
-            document.body.style.overflow = 'auto';
+            unBlockBody();
         }
     });
     
